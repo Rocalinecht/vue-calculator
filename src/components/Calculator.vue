04b38cb09b4ac3e8869feb2d4e0a63ca62dc7374
Caroline <carolinechatelon@hotmail.fr>
2020-11-26 14:27:42 +0100

    design style css

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,29 +1,29 @@
 <template>
   <div class="calculator">
     <div class="display">0.5678</div>
-    <div>AC</div>
-    <div>+/-</div>
-    <div>%</div>
-    <div>÷</div>
+    <div class="btn">AC</div>
+    <div class="btn">+/-</div>
+    <div class="btn">%</div>
+    <div class="btn operator">÷</div>
 
-    <div>7</div>
-    <div>8</div>
-    <div>9</div>
-    <div>X</div>
+    <div class="btn">7</div>
+    <div class="btn">8</div>
+    <div class="btn">9</div>
+    <div class="btn operator">X</div>
 
-    <div>4</div>
-    <div>5</div>
-    <div>6</div>
-    <div>-</div>
+    <div class="btn">4</div>
+    <div class="btn">5</div>
+    <div class="btn">6</div>
+    <div class="btn operator">-</div>
 
-    <div>1</div>
-    <div>2</div>
-    <div>3</div>
-    <div>+</div>
+    <div class="btn">1</div>
+    <div class="btn">2</div>
+    <div class="btn">3</div>
+    <div class="btn operator">+</div>
 
-    <div>0</div>
-    <div>,</div>
-    <div>=</div>
+    <div class="btn zero">0</div>
+    <div class="btn">,</div>
+    <div class="btn operator">=</div>
   </div>
 </template>
 
@@ -36,14 +36,31 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
+
 .calculator{
+  width: 400px;
+  margin: 0 auto;
   font-size: 40px;
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   grid-auto-columns: minmax(50px, auto);
+  font-family: 'Poppins', sans-serif;
 }
 .display{
   grid-column: 1 /5;
-  background-color: aqua;
+  background-color: #1C1C1C;
+  color: #D4D4D2;
+}
+.zero{
+  grid-column: 1 / 3;
+}
+.btn{
+  background-color: #D4D4D2;
+  border: 1px solid #1C1C1C;
+}
+.operator{
+  background-color: #FF9500;
+  color: white;
 }
 </style>
