a5602b2e71d4a3e27f653cf07e4bb6a88a67fa69
Caroline <carolinechatelon@hotmail.fr>
2020-11-26 14:56:32 +0100

    make method simple operator

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -4,25 +4,25 @@
     <div @click="clear" class="btn">AC</div>
     <div @click="sign" class="btn">+/-</div>
     <div  @click="percent" class="btn">%</div>
-    <div class="btn operator">÷</div>
+    <div  @click="divide" class="btn operator">÷</div>
 
     <div @click="append('7')" class="btn">7</div>
     <div @click="append('8')" class="btn">8</div>
     <div @click="append('9')" class="btn">9</div>
-    <div class="btn operator">X</div>
+    <div @click="times" class="btn operator">X</div>
 
     <div @click="append('4')" class="btn">4</div>
     <div @click="append('5')" class="btn">5</div>
     <div @click="append('6')" class="btn">6</div>
-    <div class="btn operator">-</div>
+    <div @click="minus" class="btn operator">-</div>
 
     <div @click="append('1')" class="btn">1</div>
     <div @click="append('2')" class="btn">2</div>
     <div @click="append('3')" class="btn">3</div>
-    <div class="btn operator">+</div>
+    <div @click="add" class="btn operator">+</div>
 
     <div @click="append('0')" class="btn zero">0</div>
-    <div class="btn">,</div>
+    <div @click="dot" class="btn">,</div>
     <div class="btn operator">=</div>
   </div>
 </template>
@@ -32,6 +32,7 @@ export default {
   data(){
     return{
       current:'',
+      operator: null,
     }
   },
   methods:{
@@ -47,7 +48,24 @@ export default {
     },
     append(number){
       this.current =  `${this.current} ${number}`;
-    }
+    },
+    dot(){
+      if(this.current.indexOf('.') === -1){
+          this.append('.')
+      }
+    },
+    divide(){
+      this.operator = (a, b) => a / b;
+    },
+    times(){
+      this.operator = (a, b) => a * b;
+    },
+    minus(){
+      this.operator = (a, b) => a - b;
+    },
+    add(){
+      this.operator = (a, b) => a + b;
+    }, 
   }
   
 }
@@ -70,8 +88,8 @@ export default {
   grid-column: 1 /5;
   background-color: #1C1C1C;
   color: #D4D4D2;
-  text-align: end;
-  padding-right: 10%;
+  /* text-align: end;
+  padding-right: 10%; */
 }
 .zero{
   grid-column: 1 / 3;
