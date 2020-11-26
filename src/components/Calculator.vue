9b42dfb93cf36b147619aa8b42cf8b9e24f96b74
Caroline <carolinechatelon@hotmail.fr>
2020-11-26 14:16:20 +0100

    make touch

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,32 +1,49 @@
 <template>
-  <div>
-    <h1>Hello Calculator</h1>
+  <div class="calculator">
+    <div class="display">0.5678</div>
+    <div>AC</div>
+    <div>+/-</div>
+    <div>%</div>
+    <div>÷</div>
+
+    <div>7</div>
+    <div>8</div>
+    <div>9</div>
+    <div>X</div>
+
+    <div>4</div>
+    <div>5</div>
+    <div>6</div>
+    <div>-</div>
+
+    <div>1</div>
+    <div>2</div>
+    <div>3</div>
+    <div>+</div>
+
+    <div>0</div>
+    <div>,</div>
+    <div>=</div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  
+  
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.calculator{
+  font-size: 40px;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-auto-columns: minmax(50px, auto);
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.display{
+  grid-column: 1 /5;
+  background-color: aqua;
 }
 </style>
